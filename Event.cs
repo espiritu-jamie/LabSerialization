ae30b7d0e29c4b0c4288197fe4bca5b47c3623e8
Jamie <espiritu_jamie@hotmail.com>
2023-03-16 21:23:59 -0600

    Add project files.

---
+++ Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSerialization
+{
+    [Serializable]
+    internal class Event
+    {
+        public int EventNumber
+        {
+            get;
+            set;
+        }
+
+        public string Location
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Event firstEvent = new Event()
+            {
+                EventNumber = 1,
+                Location = "Calgary"
+            };
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("event.txt", FileMode.Create))
+            {
+                formatter.Serialize(fs, firstEvent);
+                fs.Close();
+            }
+
+
+            //BinaryFormatter formatter = new BinaryFormatter();
+
+            //using (FileStream fs = new FileStream("event.txt", FileMode.Open))
+            //{
+            //    Event firstEvent = (Event)formatter.Deserialize(fs);
+            //    Console.WriteLine(firstEvent.EventNumber);
+            //    Console.WriteLine(firstEvent.Location);
+            //    fs.Close();
+            //}
+
+            //ReadFromFile();
+        }
+
+        //static void ReadFromFile()
+        //{
+        //    using (FileStream fileStream = new FileStream("event.txt", FileMode.Open))
+        //    {
+        //        StreamWriter writer = new StreamWriter(fileStream);
+        //        writer.WriteLine("Hackathon");
+        //        writer.Flush();
+
+        //        StreamReader reader = new StreamReader(fileStream);
+        //        string line = reader.ReadLine();
+
+        //        Console.WriteLine("In Word: " + line);
+
+        //        fileStream.Seek(0, SeekOrigin.Begin);
+        //        Console.WriteLine("The First Character is: " + Convert.ToChar(fileStream.ReadByte()));
+
+        //        fileStream.Seek((line.Length / 2), SeekOrigin.Begin);
+        //        Console.WriteLine("The Middle Character is: " + Convert.ToChar(fileStream.ReadByte()));
+
+        //        fileStream.Seek(line.Length - 1, SeekOrigin.Begin);
+        //        Console.WriteLine("The Last Character is: " + Convert.ToChar(fileStream.ReadByte()));
+
+        //        fileStream.Close();
+        //    }
+        //}
+    }
+}
